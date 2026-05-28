7926df371293949e813e5d98c4e6f34eef8ab73a
Vasve26 <aasvestas2013@gmail.com>
2026-05-28 13:19:47 +0300

    Add base header for main GameMode class

---
+++ Source/Vaylox/Vaylox.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VayloxGameModeBase.generated.h"
+
+UCLASS()
+class VAYLOX_API AVayloxGameModeBase : public AGameModeBase
+{
+    GENERATED_BODY()
+};
