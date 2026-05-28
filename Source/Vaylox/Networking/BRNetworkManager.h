b93f42072d7c122bc019f4b14108bedfc87c8416
Vasve26 <aasvestas2013@gmail.com>
2026-05-28 19:22:20 +0300

    Add base networking/session manager header

---
+++ Source/Vaylox/Networking/BRNetworkManager.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "BRNetworkManager.generated.h"
+
+UCLASS()
+class VAYLOX_API UBRNetworkManager : public UObject
+{
+    GENERATED_BODY()
+public:
+    UBRNetworkManager();
+    // Networking/session logic will be added here
+};
